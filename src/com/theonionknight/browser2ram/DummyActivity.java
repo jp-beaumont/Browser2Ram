e4196fbe783429bc5645f803c11212c774c1ead6
Julian Beaumont <jp.beaumont.git@theonionknight.com>
2012-04-26 06:11:02 +0000

    Fix a bug. Android doesn't send broadcasts to apps that haven't been run by the user before.

---
+++ src/com/theonionknight/browser2ram/DummyActivity.java
@@ -0,0 +1,13 @@
+package com.theonionknight.browser2ram;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DummyActivity extends Activity
+{
+    @Override public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
